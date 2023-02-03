13d4ac0657dd2fada5ef28a1cf890edb8a6c2cfa
Stephen J Carnam <steveorevo@gmail.com>
2023-02-03 15:51:33 -0800

    wip

--- plugin.php
+++ plugin.php
@@ -13,7 +13,15 @@ $hcpp->register_uninstall_script( dirname(__FILE__) . '/uninstall' );
 
 // Look for nodered.config.js app to run instead of app.config.js
 $hcpp->add_action( 'start_nodeapp_services', function( $args ) {
-    global $hcpp;
-    $hcpp->log( $args );
+    $docroot = $args[4];
+    $cmd = $args[5];
+
+    if ( file_exists( $docroot . '/nodeapp/nodered.config.js' ) ) {
+        $cmd = str_replace( 'app.config.js', 'nodered.config.js', $cmd );
+        $args[5] = $cmd;
+
+        global $hcpp;
+        $hcpp->log( 'NodeRED: Using nodered.config.js instead of app.config.js' );
+    }
     return $args;
 }, 20);
