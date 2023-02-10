4866ea58cb85fc46f7269baea8f3035f5fe77a91
Stephen J Carnam <steveorevo@gmail.com>
2023-02-10 12:28:15 -0800

    wip

--- Installers/NodeRED/NodeREDSetup.php
+++ Installers/NodeRED/NodeREDSetup.php
@@ -2,31 +2,41 @@
 
 namespace Hestia\WebApp\Installers\NodeRED;
 
+use Hestia\System\Util;
 use Hestia\WebApp\Installers\BaseSetup as BaseSetup;
+use function Hestiacp\quoteshellarg\quoteshellarg;
 
 class NodeREDSetup extends BaseSetup {
 	protected $appInfo = [
-		"name" => "Node-RED",
+		"name" => "NodeRED",
 		"group" => "framework",
 		"enabled" => true,
 		"version" => "3.0.2",
 		"thumbnail" => "nr-thumb.png",
 	];
 
-	protected $appname = "Node-RED";
-
+	protected $appname = "nodered";
 	protected $config = [
-		"form" => [],
+		"form" => [
+			"nodered_username" => ["value" => "nradmin"],
+			"nodered_password" => "password",
+			"install_directory" => ["type" => "text", "value" => "", "placeholder" => "/"],
+			"projects" => ["type" => "boolean", "value" => false, "label" => "Enable Git Projects"],
+		],
 		"database" => false,
-		"resources" => [],
+		"resources" => [
+		],
 		"server" => [
 			"nginx" => [],
-			"php" => [],
+			"php" => [
+				"supported" => ["7.3", "7.4", "8.0", "8.1", "8.2"],
+			],
 		],
 	];
 
-	public function install( array $options = null ): bool {
-		parent::install( $options );
-        return true;
+	public function install(array $options = null) {
+		parent::install($options);
+		parent::setup($options);
+		return true;
 	}
 }
