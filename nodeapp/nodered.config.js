1c6d2c2fc8a1cfc5b7a3559148fc75acc665d1dd
Stephen J Carnam <steve@steveorevo.com>
2023-09-21 22:21:15 -0700

    Ref. actual script, supports debug mode

--- nodeapp/nodered.config.js
+++ nodeapp/nodered.config.js
@@ -16,7 +16,8 @@ module.exports = {
         if (fs.existsSync(file)) {
             ver = fs.readFileSync(file, {encoding:'utf8', flag:'r'}).trim();
         }
-        nodeapp.script = '/opt/node-red/' + ver + '/bin/node-red';
+        nodeapp.ver = ver;
+        nodeapp.script = '/opt/node-red/' + ver + '/lib/node_modules/node-red/red.js';
         nodeapp.args += ' -u ' + __dirname;
         return [nodeapp];
     })()
